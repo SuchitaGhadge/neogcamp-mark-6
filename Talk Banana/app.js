488e8190ad61ddb3828b16d598088fb64816097c
SuchitaGhadge <suchita8494.sg@gmail.com>
2020-12-21 17:49:24 +0530

    style: Added style Docs:  Added js and completed the translate app

--- Talk Banana/app.js
+++ Talk Banana/app.js
@@ -1,13 +1,35 @@
-var txtInput = document.querySelector("#txt-input");
-var btnTranslate = document.querySelector("#btn-translate");
-var outputDiv = document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate"); //button
+var txtInput = document.querySelector("#txt-input"); //inputfield
+var outputDiv = document.querySelector("#output"); //output
 
-console.log(txtInput.value)
+// var serverULR = "https://lessonfourapi--tanaypratap.repl.co/translate/yoda.json"
+var serverULR = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler(){
-    outputDiv.innerText = "Your are awesome " + txtInput.value;
+function getTranslationURL(text) {
+    return serverULR + "?" + "text=" + text
+}
 
+function errorHandler(error) {
+    console.log("Error occured !" + error)
+    alert("Somethig wrong! Try again after sometime.")
+}
+
+function clickHandler() {
+    // outputDiv.innerText = "Your are awesome " + txtInput.value;
+    var inputText = txtInput.value //taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+
+        //error handling
+        .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickHandler);
 
+// https://lessonfourapi--tanaypratap.repl.co/translate/yoda.json   -> mock server link
